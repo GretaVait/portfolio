e38ce67150e56b49df1dcc2cabce8cb4c817e80c
GretaVait895 <gretavait@gmail.com>
2022-01-19 11:08:32 +0200

    added header

---
+++ components/Header.jsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__logo">
+
+          </div>
+          <div className="header__menu" role="button"><h5>Menu</h5></div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,23 @@
+// Base
 import Head from 'next/head'
 import Image from 'next/image'
+
+// Components
+import Header from '@components/Header'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Portfolio</title>
+        <title>Front-End Developer Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <Header />
+
+      <main>
+
+      </main>
     </div>
   )
 }
