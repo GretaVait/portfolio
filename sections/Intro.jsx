f107beaf43fbe7bacce994644a24b404c5803479
GretaVait895 <gretavait@gmail.com>
2022-02-03 12:17:22 +0200

    typewriter effect added

--- sections/Intro.jsx
+++ sections/Intro.jsx
@@ -1,11 +1,21 @@
+// Packages
+import Typewriter from 'typewriter-effect'
 
 const Intro = () => {
 
   return (
     <section className="intro" id="intro">
       <div className="container">
-        <h5 className="intro__subtitle">Hello, my name is Greta and I am</h5>
-        <h1 className="intro__title">Front-End Web Developer</h1>
+        <h5 className="intro__subtitle">Hello, my name is Greta and </h5>
+        <h1 className="intro__title">
+          <Typewriter
+            options={{
+              strings: ['I am Front-End Web Developer', 'I love responsive design', 'I am Web Designer', 'I love React!'],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h1>
       </div>
     </section>
   )
