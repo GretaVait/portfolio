c61d2a313c804b4c7c857cc8a2cb9f5063fb9425
GretaVait895 <gretavait@gmail.com>
2022-02-17 09:01:37 +0200

    project filter

--- sections/Projects.jsx
+++ sections/Projects.jsx
@@ -1,7 +1,11 @@
+// Base
+import { useState } from "react"
+
 // Components
 import Seperator from "@components/Seperator"
 
 const Projects = () => {
+  const [filter, setFilter] = useState("web")
 
   return (
     <section className="projects" id="projects">
@@ -9,24 +13,41 @@ const Projects = () => {
 
         <div className="projects__wrapper">
           <h2 className="projects__title">Projects</h2>
-
+        
           <div className="projects__filter">
-            <div className="projects__filter__name active" role="button">
-              <h5>Web Development</h5>
-            </div>
-            <div className="projects__filter__name" role="button">
-              <h5>Mobile Development</h5>
-            </div>
-            <div className="projects__filter__name" role="button">
-              <h5>Design</h5>
-            </div>
+            <FilterItem label="Web Development" active={filter === "web"} onSelect={() => { setFilter("web") }} />
+            <FilterItem label="Mobile Development" active={filter === "mobile"} onSelect={() => { setFilter("mobile") }} />
+            <FilterItem label="Design" active={filter === "design"} onSelect={() => { setFilter("design") }} />
           </div>
         </div>
 
         <Seperator />
+
+        <div className="projects__list">
+          <ProjectCard label="1" active={filter === "web"} />
+          <ProjectCard label="2" active={filter === "mobile"} />
+          <ProjectCard label="3" active={filter === "design"} />
+          <ProjectCard label="4" active={filter === "web"} />
+          <ProjectCard label="5" active={filter === "mobile"} />
+          <ProjectCard label="6" active={filter === "design"} />
+        </div>
       </div>
     </section>
   )
 }
+
+const FilterItem = ({ label, active, onSelect }) => {
+  return (
+    <div className={`projects__filter__name ${active ? "active" : ""}`} onClick={onSelect} role="button">
+      <h5>{label}</h5>
+    </div>
+  )
+}
+
+const ProjectCard = ({ label, active }) => (
+  <div className={`projects__list__card ${active ? 'active' : ''}`}>
+    {label}
+  </div>
+)
 
 export default Projects
